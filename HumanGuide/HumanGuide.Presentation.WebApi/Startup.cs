3135350569fc229e5cc1c0539cd48e34a988d069
nikdolidze <n.dolidze97@gmail.com>
2021-10-20 23:45:01 +0400

    Create a separate Projects for each layer

--- HumanGuide/HumanGuide.Presentation.WebApi/Startup.cs
+++ HumanGuide/HumanGuide.Presentation.WebApi/Startup.cs
@@ -30,7 +30,7 @@ namespace HumanGuide.Presentation.WebApi
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HumanGuide.Presentation.WebApi", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace HumanGuide.Presentation.WebApi
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HumanGuide.Presentation.WebApi v1"));
             }
 
             app.UseHttpsRedirection();
